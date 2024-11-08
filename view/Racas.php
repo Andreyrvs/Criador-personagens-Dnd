e888bad202aebfdfb6aac79470492b0af8a6aa9c
Andrey R. Visniewski <andreyrvdev@gmail.com>
2024-11-08 18:11:17 -0300

    Modifica foreach para deixar mais legivel

--- view/Racas.php
+++ view/Racas.php
@@ -3,6 +3,27 @@ session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include 'controller/RacasController.php';
 }
+
+$racas_lista = array(
+    "Alto Elfo" => "Inteligência (+1)",
+    "Anao" => "Constituição (+2)",
+    "Anao da Colina" => "Sabedoria (+1)",
+    "Anao da Montanha" => "Força (+2)",
+    "Draconato" => "Força (+2) Carisma (+1)",
+    "Drow" => "Carisma (+1)",
+    "Elfo" => "Destreza (+2)",
+    "Elfo da Floresta" => "Sabedoria (+1)",
+    "Gnomo" => "Inteligência (+2)",
+    "Gnomo da Floresta" => "Destreza (+1)",
+    "Gnomo das Rochas" => "Constituição (+1)",
+    "Halfling" => "Destreza (+2)",
+    "Halfling Pés Leves" => "Carisma (+1)",
+    "Halfling Robusto" => "Constituição (+1)",
+    "Humano" => "Todos (+1,+1,+1,+1,+1,+1)",
+    "Meio Elfo" => "Carisma (+2)",
+    "Meio Orc" => "Força(+2) Constituição (+1)",
+    "Tiefling" => "Inteligência(+1) Carisma (+2)"
+);
 ?>
 
 <!DOCTYPE html>
@@ -23,24 +44,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <label for="racas">Selecione uma raça: </label>
                 <select id="racas" name="racas" required>
                     <option value="" selected disabled>Opções</option>
-                    <option value="Alto Elfo">Alto Elfo</option>
-                    <option value="Anao">Anao</option>
-                    <option value="Anao da Colina">Anao da Colina</option>
-                    <option value="Anao da Montanha">Anao da Montanha</option>
-                    <option value="Draconato">Draconato</option>
-                    <option value="Drow">Drow</option>
-                    <option value="Elfo">Elfo</option>
-                    <option value="Elfo da Floresta">Elfo da Floresta</option>
-                    <option value="Gnomo">Gnomo</option>
-                    <option value="Gnomo da Floresta">Gnomo da Floresta</option>
-                    <option value="Gnomo das Rochas">Gnomo das Rochas</option>
-                    <option value="Halfling">Halfling</option>
-                    <option value="Halfling PesLeves">Halfling PesLeves</option>
-                    <option value="Halfling Robusto">Halfling Robusto</option>
-                    <option value="Humano">Humano</option>
-                    <option value="Meio Elfo">Meio Elfo</option>
-                    <option value="Meio Orc">Meio Orc</option>
-                    <option value="Tiefling">Tiefling</option>
+                    <?php foreach ($racas_lista as $raca => $valor): ?>
+                        <option value="<?php echo $raca ?>"> <?php echo $raca ?> </option>
+                    <?php endforeach; ?>
                 </select>
                 <button type="submit">Enviar</button>
             </section>
@@ -59,81 +65,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>Alto Elfo</td>
-                    <td>Inteligência (+1)</td>
-                </tr>
-                <tr>
-                    <td>Anao</td>
-                    <td> Constituição (+2)</td>
-                </tr>
-                <tr>
-                    <td>Anao da Colina</td>
-                    <td>Sabedoria (+1)</td>
-                </tr>
-                <tr>
-                    <td>Anao da Montanha</td>
-                    <td>Força (+2)</td>
-                </tr>
-                <tr>
-                    <td>Draconato</td>
-                    <td>Força (+2) Carisma (+1)</td>
-                    <p></p>
-                </tr>
-                <tr>
-                    <td>Drow</td>
-                    <td>Carisma (+1)</td>
-                </tr>
-                <tr>
-                    <td>Elfo</td>
-                    <td>Destreza (+2)</td>
-                </tr>
-                <tr>
-                    <td>Elfo da Floresta</td>
-                    <td>Sabedoria (+1)</td>
-                </tr>
-                <tr>
-                    <td>Gnomo</td>
-                    <td>Inteligência (+2)</td>
-                </tr>
-                <tr>
-                    <td>Gnomo da Floresta</td>
-                    <td>Destreza (+1)</td>
-                </tr>
-                <tr>
-                    <td>Gnomo das Rochas</td>
-                    <td>Constituição (+1)</td>
-                </tr>
-                <tr>
-                    <td>Halfling</td>
-                    <td>Destreza (+2)</td>
-                </tr>
-                <tr>
-                    <td>Halfling Pés Leves</td>
-                    <td>Carisma (+1)</td>
-                </tr>
-                <tr>
-                    <td>Halfling Robusto</td>
-                    <td>Constituição (+1)</td>
-                </tr>
-                <tr>
-                    <td>Humano</td>
-                    <td>
-                        Todos (+1)
-                    </td>
-                </tr>
-                <tr>
-                    <td>Meio Elfo</td>
-                    <td>Carisma (+2)</td>
-                </tr>
-                <tr>
-                    <td>Meio Orc</td>
-                    <td>Força(+2) Constituição (+1)</td>
-                </tr>
-                <tr>
-                    <td>Tiefling</td>
-                    <td>inteligência(+1) Carisma(+2)</td>
-                </tr>
+                <?php foreach ($racas_lista as $raca => $valor): ?>
+                    <tr>
+                        <td> <?php echo $raca ?> </td>
+                        <td> <?php echo $valor ?> </td>
+                    </tr>
+                <?php endforeach; ?>
             </tbody>
         </table>
     </section>
--- view/Resumo.php
+++ view/Resumo.php
@@ -3,12 +3,13 @@ session_start();
 require_once 'model\Distribuir27Pontos.php'; // Adicionando a inclusão da classe
 
 if (isset($_SESSION['racas'])) {
-    $racas = unserialize($_SESSION["racas"]);
+    $raca = unserialize($_SESSION["racas"]);
 }
 if (isset($_SESSION['distribuirPontos'])) {
     $distribuirPontos = unserialize($_SESSION['distribuirPontos']);
     $atributos = $distribuirPontos->getAtributos();
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -24,19 +25,19 @@ if (isset($_SESSION['distribuirPontos'])) {
     <h1>Resumo</h1>
     <section class="page">
         <section class="card">
-            <h1>Personagem</h1>
+            <h2>Personagem</h2>
             <section>
-                <span><?php echo $racas ?></span>
+                <p><?php echo $raca ?></p>
             </section>
             <section class="habilidades">
-                <?php
-                echo "<h3>Atributos:</h3>";
-                echo "<ul>";
-                foreach ($distribuirPontos->getAtributos() as $atributo => $valor) {
-                    echo "<li>$atributo: $valor</li>";
-                }
-                echo "</ul>";
-                ?>
+                <h3>Atributos</h3>
+                <?php foreach ($distribuirPontos->getAtributos() as $atributo => $valor): ?>
+                    <ul>
+                        <li>
+                            <?php echo "{$atributo} : {$valor}" ?>
+                        </li>
+                    </ul>
+                <?php endforeach ?>
             </section>
         </section>
     </section>
