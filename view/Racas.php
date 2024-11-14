cb0a8a46d111e9e1a289545fe55f608b92de55bf
Andrey R. Visniewski <andreyrvdev@gmail.com>
2024-11-14 18:00:53 -0300

    remove o array $racas_lista e torna o view independente.

--- view/Racas.php
+++ view/Racas.php
@@ -4,32 +4,10 @@ require_once 'controller/RacasController.php';
 $controller = new RacasController();
 $controller->selecionarRaca();
 
-$racas_lista = array(
-    "AltoElfo" => "Inteligência (+1)",
-    "Anao" => "Constituição (+2)",
-    "AnaoDaColina" => "Sabedoria (+1)",
-    "AnaoDaMontanha" => "Força (+2)",
-    "Draconato" => "Força (+2) Carisma (+1)",
-    "Drow" => "Carisma (+1)",
-    "Elfo" => "Destreza (+2)",
-    "ElfoDaFloresta" => "Sabedoria (+1)",
-    "Gnomo" => "Inteligência (+2)",
-    "GnomoDaFloresta" => "Destreza (+1)",
-    "GnomoDasRochas" => "Constituição (+1)",
-    "Halfling" => "Destreza (+2)",
-    "HalflingPesLeves" => "Carisma (+1)",
-    "HalflingRobusto" => "Constituição (+1)",
-    "Humano" => "Todos (+1,+1,+1,+1,+1,+1)",
-    "MeioElfo" => "Carisma (+2)",
-    "MeioOrc" => "Força(+2) Constituição (+1)",
-    "Tiefling" => "Inteligência(+1) Carisma (+2)"
-);
-
 function adicionarEspacos($string)
 {
     return preg_replace('/([a-z])([A-Z])/u', '$1 $2', $string);
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -52,39 +30,33 @@ function adicionarEspacos($string)
             <section class="formulario-interno">
                 <select id="raca" name="raca" required>
                     <option value="" selected disabled>Opções</option>
-                    <?php foreach ($racas_lista as $raca => $valor): ?>
-                        <?php echo $valor ?>
-                        <option value="<?php echo $raca ?>"> <?php echo adicionarEspacos($raca) ?> </option>
+                    <?php foreach ($controller->getRacas() as $raca): ?>
+                        <option value="<?php echo $raca->getNome(); ?>"> <?php echo adicionarEspacos($raca->getNome()); ?> </option>
                     <?php endforeach; ?>
                 </select>
                 <button type="submit" value="Selecionar Raça">Criar</button>
             </section>
         </form>
 
-
         <section class="tabela-racas">
             <table>
                 <thead>
-                    <caption>
-                        Súmario: Valores de Habilidade
-                    </caption>
+                    <caption>Súmario: Valores de Habilidade</caption>
                     <tr class="cabeçalho">
                         <th>Raças</th>
                         <th>Aprimoramentos Raciais:</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <?php foreach ($racas_lista as $raca => $valor): ?>
+                    <?php foreach ($controller->getRacas() as $raca): ?>
                         <tr>
-                            <td> <?php echo adicionarEspacos($raca) ?> </td>
-                            <td> <?php echo $valor ?> </td>
+                            <td> <?php echo adicionarEspacos($raca->getNome()); ?> </td>
+                            <td> <?php echo $raca->getAprimoramento(); ?> </td>
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
             </table>
         </section>
-
-
     </main>
 </body>
 
