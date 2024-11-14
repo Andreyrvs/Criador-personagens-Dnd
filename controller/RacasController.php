0a08051bdc222e09871c7f84bfab98ecd4c7b2b2
Andrey R. Visniewski <andreyrvdev@gmail.com>
2024-11-14 09:53:30 -0300

    remove comentarios, e melhora a classe

--- controller/RacasController.php
+++ controller/RacasController.php
@@ -1,41 +1,30 @@
 <?php
-
-
-require_once "model/Racas/interface/InterfaceRaca.php";
-
 class RacasController
 {
+    private $racaSelecionada;
+    private $caminhoRaca;
+    private $classeRaca;
+
+    public function __construct()
+    {
+        require_once "model/Racas/interface/InterfaceRaca.php";
+    }
+
     public function selecionarRaca()
     {
-        // Verifique se a requisição é do tipo POST
         if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['raca'])) {
-            $racaSelecionada = $_POST['raca'];
+            $this->racaSelecionada = $_POST['raca'];
+            $this->caminhoRaca = "model/Racas/{$this->racaSelecionada}.php";
+            if (file_exists($this->caminhoRaca)) {
+                require_once $this->caminhoRaca;
+                $this->classeRaca = new $this->racaSelecionada();
+                $_SESSION['racas'] = serialize($this->classeRaca);
 
-            // Construa o caminho do arquivo da raça
-            $caminhoRaca = "model/Racas/{$racaSelecionada}.php";
-
-            // Verifique se o arquivo existe antes de incluí-lo
-            if (file_exists($caminhoRaca)) {
-                require_once $caminhoRaca;
-
-                // Instancie a classe da raça
-                $classeRaca = new $racaSelecionada();
-                $_SESSION['racas'] = serialize($classeRaca);
-
-                // Redirecione ou carregue a view apropriada
-                header("Location: Resumo.php"); // Exemplo de redirecionamento
+                header("Location: Resumo.php");
                 exit();
             } else {
-                // Trate o erro se a raça não existir
                 echo "Raça não encontrada.";
             }
-        } else {
-            // Se não for uma requisição POST, redirecione ou exiba uma mensagem
-            echo "Selecione uma raça.";
         }
     }
 }
-
-// Crie uma instância do controlador e chame o método
-$controller = new RacasController();
-$controller->selecionarRaca();
