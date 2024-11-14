fa251113e615d1afd7ac7819a59f84d46b466df9
Andrey R. Visniewski <andreyrvdev@gmail.com>
2024-11-14 09:26:31 -0300

    Modifica Controller para usar Classe. modifica a chamada no resumo.php

--- controller/ResumoController.php
+++ controller/ResumoController.php
@@ -1,22 +1,80 @@
 <?php
 
-include_once 'model/Racas.php';
+class ResumoController
+{
+    private $raca;
+    private $pontosFinaisComBonus;
+    private $pontosVida;
+    private $atributosRaca;
+    private $modificado;
+    private $modificadores;
+    private $distribuirPontos;
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    public function __construct()
+    {
+        require_once 'model/Distribuir27Pontos.php';
+        require_once "model/AplicarBonusRaca.php";
 
-    $erros = FALSE;
-    $chaveRaca = $_POST['racas'];
-    if (isset($_POST['racas'])) {
-        if (!$chaveRaca) {
-            echo "<br> Raça inválida para $chaveRaca<br>";
-            $erros = TRUE;
-        } #else
-        # echo "<br> $chaveRaca ok<br>";
+        spl_autoload_register(function ($class_name) {
+            $file = __DIR__ . '/../model/Racas/' . $class_name . '.php';
+            if (file_exists($file)) {
+                require_once $file;
+            }
+        });
     }
-}
 
-if (!$erros) {
-    $_SESSION['racas'] = serialize($chaveRaca);
-    header("Location: Resumo.php");
-    exit();
+    public function processarDados()
+    {
+        $this->validarRacaSessao();
+        $this->validarCaminhoDoArquivo();
+        $this->validarDistribuir27PontosSessao();
+
+        return [
+            'raca' => $this->raca,
+            'pontosFinaisComBonus' => $this->pontosFinaisComBonus,
+            'pontosVida' => $this->pontosVida,
+            'atributosRaca' => $this->atributosRaca,
+            'modificado' => $this->modificado
+        ];
+    }
+
+    public function validarRacaSessao()
+    {
+        if (isset($_SESSION['racas'])) {
+            $this->raca = unserialize($_SESSION['racas']);
+        } else {
+            echo "Nenhuma raça selecionada.";
+            exit();
+        }
+    }
+
+    public function validarCaminhoDoArquivo()
+    {
+        if ($this->raca && class_exists($this->raca->getNome())) {
+            $caminhoRaca = __DIR__ . '/../model/Racas/' . $this->raca->getNome() . '.php';
+            if (file_exists($caminhoRaca)) {
+                require_once $caminhoRaca;
+            }
+            $this->atributosRaca = $this->raca->getAtributos();
+        } else {
+            echo "Classe da raça não encontrada.";
+            exit();
+        }
+    }
+
+    public function validarDistribuir27PontosSessao()
+    {
+        if (isset($_SESSION['distribuirPontos'])) {
+            $this->distribuirPontos = unserialize($_SESSION['distribuirPontos']);
+            $this->distribuirPontos->setModificadores();
+            $this->modificado = $this->distribuirPontos->getModificadores();
+        } else {
+            echo "Nenhuma distribuição de pontos encontrada.";
+            exit();
+        }
+
+        $aplicarBonusRaca = new AplicarBonusRaca($this->distribuirPontos);
+        $this->pontosFinaisComBonus = $aplicarBonusRaca->aplicarBonus($this->raca);
+        $this->pontosVida = $this->distribuirPontos->setPontosVida($this->pontosFinaisComBonus);
+    }
 }
--- view/Resumo.php
+++ view/Resumo.php
@@ -1,48 +1,16 @@
 <?php
 session_start();
-// Auto loader
-spl_autoload_register(function ($class_name) {
-    $file = __DIR__ . '/../model/Racas/' . $class_name . '.php';
-    if (file_exists($file)) {
-        require_once $file;
-    }
-});
+require_once 'controller/ResumoController.php';
 
-require_once 'model\Distribuir27Pontos.php';
-require_once "model\AplicarBonusRaca.php";
+$controller = new ResumoController();
+$dados = $controller->processarDados();
 
-if (isset($_SESSION['racas'])) {
-    $raca = unserialize($_SESSION["racas"]);
-    if (class_exists($raca->getNome())) {
-        $caminhoRaca = "model/Racas/{$raca->getNome()}.php";
-        if (file_exists($caminhoRaca)) {
-            require_once $caminhoRaca;
-        }
-        $atributosRaca = $raca->getAtributos();
-    } else {
-        echo "Classe da raça não encontrada.";
-    }
-} else {
-    echo "Nenhuma raça selecionada.";
-}
-
-if (isset($_SESSION['distribuirPontos'])) {
-    $distribuirPontos = unserialize($_SESSION['distribuirPontos']);
-    $atributosDistribuidos = $distribuirPontos->getAtributos();
-    $modificadores = $distribuirPontos->setModificadores();
-    $modificado = $distribuirPontos->getModificadores();
-
-    $atributosModificados = $distribuirPontos->getModificadores();
-}
+extract($dados);
 
 function adicionarEspacos($string)
 {
     return preg_replace('/([a-z])([A-Z])/u', '$1 $2', $string);
 }
-
-$aplicarBonusRaca = new AplicarBonusRaca($distribuirPontos);
-$pontosFinaisComBonus = $aplicarBonusRaca->aplicarBonus($raca);
-$pontosVida = $distribuirPontos->setPontosVida($pontosFinaisComBonus);
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -59,51 +27,52 @@ $pontosVida = $distribuirPontos->setPontosVida($pontosFinaisComBonus);
     <section class="page">
         <section class="card">
             <h2>Personagem</h2>
-            <section>
-                <h3><?php echo adicionarEspacos($raca->getNome()); ?></h3>
-            </section>
+            <?php if (isset($raca)): ?>
+                <section>
+                    <h3><?php echo adicionarEspacos($raca->getNome()); ?></h3>
+                </section>
+            <?php endif; ?>
+
             <section class="habilidades">
                 <h4>Atributos</h4>
-                <ul>
-                    <?php foreach ($pontosFinaisComBonus as $atributo => $valor): ?>
-                        <li>
-                            <?php echo "{$atributo} : {$valor}" ?>
-                        </li>
-                    <?php endforeach ?>
-                </ul>
+                <?php if (isset($pontosFinaisComBonus)): ?>
+                    <ul>
+                        <?php foreach ($pontosFinaisComBonus as $atributo => $valor): ?>
+                            <li><?php echo "{$atributo} : {$valor}" ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+
+                <?php if (isset($pontosVida)): ?>
+                    <section>
+                        <p>Pontos de Vida: <span><?php echo $pontosVida ?></span></p>
+                    </section>
+                <?php endif; ?>
+
+                <?php if (isset($atributosRaca)): ?>
+                    <p>Aprimoramentos raciais</p>
+                    <ul>
+                        <?php foreach ($atributosRaca as $tes => $res): ?>
+                            <?php if ($res !== 0): ?>
+                                <li><?php echo $tes . ' ' . $res; ?></li>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+            </section>
+
+            <?php if (isset($modificado)): ?>
                 <section>
-                    <p>Pontos de Vida: <span><?php echo $pontosVida ?></span></p>
+                    <p>Antes do bônus de raça:</p>
+                    <ul>
+                        <?php foreach ($modificado as $key => $value): ?>
+                            <li><?php echo $key . ' ' . $value ?></li>
+                        <?php endforeach; ?>
+                    </ul>
                 </section>
-                <p>Aprimoramentos raciais</p>
-                <ul>
-                    <?php foreach ($atributosRaca as $tes => $res): ?>
-                        <?php
-                        if ($res !== 0): ?>
-                            <li>
-                                <?php echo $tes; ?>
-                                <?php echo  $res; ?>
-                            </li>
-                        <?php endif ?>
-                    <?php endforeach ?>
-                </ul>
-            </section>
-
-            <section>
-                <p>Antes do bônus de raça:</p>
-                <ul>
-                    <?php foreach ($modificado as $key => $value) : ?>
-                        <li>
-                            <?php echo $key ?>
-                            <?php echo $value  ?>
-                        </li>
-
-                    <?php endforeach ?>
-                </ul>
-            </section>
+            <?php endif; ?>
         </section>
     </section>
-
-
 </body>
 
 </html>
